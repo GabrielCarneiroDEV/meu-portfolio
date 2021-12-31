9bee1fef35fac1461390f6b293ad343fc611d076
Gabriel Carneiro <gabriel29music@gmail.com>
2021-12-30 23:41:23 -0300

    trabalhando na responsividade

--- src/Components/HeaderNav/index.js
+++ src/Components/HeaderNav/index.js
@@ -1,9 +1,35 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import MenuIcon from '../../Assets/menu.svg';
+import CloseIcon from '../../Assets/close.svg';
 import './styles.css';
 
 function HeaderNav() {
+  const [openMenu, setOpenMenu] = useState(false);
+
+
   return (
     <nav>
+      <img
+        src={openMenu ? CloseIcon : MenuIcon}
+        alt=''
+        className={`${openMenu ? 'close-icon' : 'menu-icon'}`}
+        onClick={() => setOpenMenu(!openMenu)}
+      />
+      {openMenu && <div className='container-menu'>
+      <div className='container-menu-nav'>
+        <Link to={'/'} className='unset'>
+          <span>Página Inicial</span>
+        </Link>
+
+        <Link to={'/Projects'} className='unset'>
+          <span>Projetos</span>
+        </Link>
+        <Link to={'/Contact'} className='unset'>
+          <span>Contato</span>
+        </Link>
+      </div>
+        </div>}
       <div className='container-nav'>
         <Link to={'/'} className='unset'>
           <span>Página Inicial</span>
